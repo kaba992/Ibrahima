1b692a1d5cdfcfcdfe3e8d06fe7f02b3edcaf51d
kaba92 <ikkaba92@hotmail.fr>
2021-09-12 00:03:42 +0200

    MAJ Header

--- Ibrahima.js
+++ Ibrahima.js
@@ -1,26 +1,26 @@
-const text =document.querySelector(".titre");
+const text = document.querySelector(".titre");
 const strText = text.textContent;
 console.log(strText);
 const splitText = strText.split("");
 text.textContent = "";
-for ( let i=0; i < splitText.length; i++){
+for (let i = 0; i < splitText.length; i++) {
     text.innerHTML += "<span>" + splitText[i] + "</span>";
 }
 
 let char = 0;
 let timer = setInterval(onTick, 100);
 
-function onTick (){
+function onTick() {
     const span = text.querySelectorAll('span')[char];
     console.log(span);
     span.classList.add('fades');
     char++
-    if (char === splitText.length){
+    if (char === splitText.length) {
         complete();
         return;
     }
 }
-function complete (){
+function complete() {
     clearInterval(timer)
     timer = null;
 }
